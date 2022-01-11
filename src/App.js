e8b32fa4a70623116da4f791505e9b8ac569890d
Daisy Koome <dskoome@gmail.com>
2022-01-11 05:18:25 +0300

    Reverted back to component creation using JSX because it makes development easier and faster

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
-const Star = () => React.createElement('div', null, 'This is a little star');
-
-const App = () => {
-  return Star();
+// reverted back to component creation using JSX because it makes development easier and faster
+function App() {
+  return (
+    <div className="App">
+      Start here.
+    </div>
+  );
 }
 
 export default App;
