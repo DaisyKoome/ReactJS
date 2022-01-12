809b6867b6d35d4b65758b5db0d1a15b167dd4c3
Daisy Koome <dskoome@gmail.com>
2022-01-13 02:30:50 +0300

    Lesson about states and props

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import styled from 'styled-components';
 import Lamp from './Lamp';
 import LightSwitch from './LightSwitch';
 
+//style component
+//styling on the room component
 const Room = styled.div`
   position: relative;
   width: 500px;
@@ -14,14 +16,26 @@ const Room = styled.div`
 
 function App() {
   // Lamp one
+  //isLampOneOn is the state name
+  //setIsLampOneOn is the setter of the state
+  //useState is the function used to set the state
+  //A state in React is immutable - cannot be modified after creation 
+  //So always use the setter to set the state 
   const [isLampOneOn, setIsLampOneOn] = useState(false);
   // Lamp two
   const [isLampTwoOn, setIsLampTwoOn] = useState(true);
 
+  //handleLightSwitchOne and handleLightSwitchTwo are functions
+  //handleLightSwitchOne changes the state of the lamp to true (on) through the setter setIsLampOneOn
   const handleLightSwitchOne = () => setIsLampOneOn(prev => !prev);
+  //handleLightSwitchTwo changes the state of the lamp to false (off) through the setter setIsLampTwoOn
   const handleLightSwitchTwo = () => setIsLampTwoOn(prev => !prev);
 
   return (
+    //Room component in JSX
+    //Lamp component
+    //lampOn prop, position prop - a prop is an object
+    //LightSwitch component
     <Room>
       <Lamp lampOn={isLampOneOn} position='left' />
       <Lamp lampOn={isLampTwoOn} position='right' />
