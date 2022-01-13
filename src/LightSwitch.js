5e6be386cb5b4a6db2762ed47f3cbf34c95d03c8
Daisy Koome <dskoome@gmail.com>
2022-01-13 03:38:52 +0300

    Additional comments about states and props

--- src/LightSwitch.js
+++ src/LightSwitch.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
+//Button styled component
 const Button = styled.button`
   position: absolute;
   left: ${props => (props.position === 'left' ? '20px' : '380px')};
@@ -14,7 +15,15 @@ const Button = styled.button`
   cursor: pointer;
 `;
 
+/*The callback prop here is sent into the LightSwitch component 
+to 'call back' the handleLightSwitchOne function from the app component in App.js 
+*/
 const LightSwitch = ({ callback, position, switchOn }) => (
+//The button has an onClick handler which will trigger the callback function
+/*
+Props make a component to be dynamic and reusable so that it can be used 
+in different situations (adapt the logic to suit the use case you want)
+*/
   <Button onClick={callback} position={position}>
     {switchOn ? 'On' : 'Off'}
   </Button>
