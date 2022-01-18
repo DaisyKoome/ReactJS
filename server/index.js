f72d45ef6542b086698e4e741f7257d87219b1ef
Daisy Koome <dskoome@gmail.com>
2022-01-18 09:30:22 +0300

    Posted a string from the server to the front end

--- server/index.js
+++ server/index.js
@@ -4,6 +4,15 @@
 const express = require('express')
 const app = express()
 
+//make the server do sth - send a hello world response to the front end on the index page/route
+//res is resource, req is require - both are parameters
+//req is used when getting info from the front end
+//res is used when posting info to the front end
+app.get('/', (req,res)=> {
+    res.send("Hello world");
+})
+
+//port, arrow function
 app.listen(3001, ()=> {
     console.log("Running on port 3001");
 });
