22a49a2e6bd4c195541cab8821d4d59bc85de43e
Daisy Koome <dskoome@gmail.com>
2022-01-18 09:09:49 +0300

    Configured the port

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
 //The server will run through this file
+//Create express server
+
+const express = require('express')
+const app = express()
+
+app.listen(3001, ()=> {
+    console.log("Running on port 3001");
+});
