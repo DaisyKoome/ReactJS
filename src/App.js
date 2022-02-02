ae952dc139ef8d9600b57a08d752cfb6168a9c9a
DaisyKoome <dskoome@gmail.com>
2022-02-02 04:21:46 +0300

    added a post request function to front end and onclick event to submit button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { useState } from "react";
+//axios replicates fetch function for making http requests
 import Axios from 'axios'
 import './App.css';
 
 function App() {
 
   //create 2 states
-  const[ usernameReg, setUsernameReg ] = useState('')
-  const[ passwordReg, setPasswordReg ] = useState('')
+  const[ usernameReg, setUsernameReg ] = useState('');
+  const[ passwordReg, setPasswordReg ] = useState('');
 
-  
+  /*create a function that makes the data go to backend 
+  (for insertion into DB) when submit button is clicked
+  - Parse in it an object with uname and pswd properties*/
+  const register = () => {
+    Axios.post('https://localhost:3001/register', {
+      //these 2 variables are being sent to the backend
+      username:usernameReg, 
+      password:passwordReg
+      //then grab the response and console.log it
+    }).then((response)=> {
+      /* if an error occurs, we'll see it 
+      if no error, a response will still be displayed*/
+      console.log(response);
+    });
+  };
 
   return (
     <div className="App">
@@ -24,7 +39,7 @@ function App() {
           onChange={(e) => {
             setPasswordReg(e.target.value);
           }} name="password"/>
-          <button>Register</button>
+          <button onClick = {register}>Register</button>
       </div>
       <div className="form login">
         <h1>Login</h1>
