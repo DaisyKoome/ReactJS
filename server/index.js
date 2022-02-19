c6a412b168f4db7312f1d175d6c9a6c1e66976cd
DaisyKoome <dskoome@gmail.com>
2022-02-19 15:59:34 +0300

    Made first table using Sequelize ORM

---
+++ server/index.js
@@ -0,0 +1,18 @@
+//The server will run through this file
+//Create express server
+
+const express = require('express')
+const app = express();
+
+//make the server do sth - send a hello world response to the front end on the index page/route
+//res is resource, req is require - both are parameters
+//req is used when getting info from the front end
+//res is used when posting info to the front end
+const db = require('./models');
+
+db.sequelize.sync().then(()=> {
+//port, arrow function
+    app.listen(3001, ()=> {
+        console.log("Running on port 3001");
+    });
+});
