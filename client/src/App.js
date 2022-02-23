0c1a7044ecc6bfc3bf1dba8bb7393eab7cbcf5ed
DaisyKoome <dskoome@gmail.com>
2022-02-23 07:05:35 +0300

    init files for the blog posts and comments app

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import axios from "axios";
+import { useEffect } from React;
+import React from 'react';
+
+function App() {
+  
+  //axios enables a user to make API requests very easily
+
+  //To make a GET Request
+  useEffect (() => {
+
+  }, [])
+  
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
